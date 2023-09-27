92a32a6c8ba22f18106a8475fb6f6900688c7b28
Steve Johnson <steve21johnson@gmail.com>
2023-09-27 12:06:21 -0700

    webhook example

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 var express = require("express");
 var router = express.Router();
+const OpenAI = require("openai");
 
+const openai = new OpenAI({
+  apiKey: "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",
+});
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/", function (req, res, next) {
-  res.send({
-    payload: {
-      sja: "sjb",
+router.post("/", async function (req, res, next) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: req.body.text },
+    ],
+  });
+  res.json({
+    fulfillmentResponse: {
+      messages: [
+        {
+          text: {
+            text: [response.choices[0].message.content],
+          },
+        },
+      ],
     },
   });
 });
